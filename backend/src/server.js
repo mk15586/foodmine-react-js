bec12cf5ab10bde8e397e8ea586061b38ba98711
Mohit Kumar <mohitk15586@gmail.com>
2024-11-21 14:04:37 +0530

    Added some Components and script to publish this project

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
 import {fileURLToPath} from 'url';
 
 import express from "express";
@@ -30,13 +31,13 @@ app.use('/api/foods',foodRouter);
 app.use('/api/users',userRouter);
 app.use('/api/orders',orderRouter);
 
-const publicFolder = path.join(__dirname,'public');
+const publicFolder = path.join(__dirname, "public");
 app.use(express.static(publicFolder));
 
-app.get('*',(req,res) => {
-    const indexFilePath = path.join(publicFolder,'index.html');
-    res.sendFile(indexFilePath);
-})
+app.get("*", (req, res) => {
+  const indexFilePath = path.join(publicFolder, "index.html");
+  res.sendFile(indexFilePath);
+});
 
 const PORT = 5000;
 app.listen(PORT, () => {
